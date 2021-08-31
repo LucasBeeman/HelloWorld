b607de153a70391f9201fba82700ffd5d186f174
lucas <lucas@LUCAS-PC>
2021-08-30 22:02:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+            Console.ReadKey();
+        }
+    }
+}
